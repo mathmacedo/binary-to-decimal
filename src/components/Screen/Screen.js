cbc3bac2a3a6133c03db1e8b22e84a7627920fde
Matheus Macedo <matheus.macedo1977@gmail.com>
2019-09-19 13:39:09 -0300

    Criando componente screen

---
+++ src/components/Screen/Screen.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Screen.css";
+
+class Screen extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {};
+	}
+
+	render() {
+		return <div></div>;
+	}
+}
+
+export default Screen;
