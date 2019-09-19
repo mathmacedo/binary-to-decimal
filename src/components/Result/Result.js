b2f72dad23d9b6fe86e5d53f3c191e3b12c48fdf
Matheus Macedo <matheus.macedo1977@gmail.com>
2019-09-19 13:41:31 -0300

    Criando componente result

---
+++ src/components/Result/Result.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Result.css";
+
+class Result extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {};
+	}
+
+	render() {
+		return <div></div>;
+	}
+}
+
+export default Result;
